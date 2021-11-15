fde0fea81d8d0c129c6e8fea2991c6ddbcfb88a1
Ken Kuo <csco10898763@gmail.com>
2021-11-15 13:54:40 +0800

    add change str to int

--- products.py
+++ products.py
@@ -4,7 +4,8 @@ while True:
     if name == 'q':
         break
     # products.append(name)
-    price = input('請輸入商品價格：')
+    price = int(input('請輸入商品價格：'))
+    # price = int(price)
     # p = [name, price]
     # p.append(name)
     # p.append(price)
@@ -19,5 +20,6 @@ for p in products:
     print(p[0], '的價格是', p[1])
 
 with open('products.csv', 'w') as f:
+    f.write('商品,價格\n')
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+        f.write(p[0] + ',' + str(p[1]) + '\n')
