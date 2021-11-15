b255e9a4c540d8c226c8b088a75367a2249367e6
Ken Kuo <csco10898763@gmail.com>
2021-11-15 14:45:15 +0800

    add open read and append csv

--- products.py
+++ products.py
@@ -1,4 +1,11 @@
 products = []
+# 讀取檔案
+with open('products.csv', 'r') as f:
+    for line in f:
+        name, price = line.strip().split(',') # strip() 去掉跳行符號 \n & 資料以 ',' 作為分割
+        products.append([name, price])
+print(products)
+
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
@@ -19,7 +26,7 @@ print(products, "\n")
 for p in products:
     print(p[0], '的價格是', p[1])
 
-with open('products.csv', 'w', encoding='utf-8') as f:
+with open('products.csv', 'w') as f:
     f.write('商品,價格\n')
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')
